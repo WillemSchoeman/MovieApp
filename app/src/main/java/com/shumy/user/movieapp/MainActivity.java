91633fa22c8b9f379c41d0f266e9a2f7e1260fe3
WillemSchoeman <willem.schoeman17@gmail.com>
2018-06-24 21:01:25 +0200

    Fixed weird spacing error on about page.
    Removed API key.

--- app/src/main/java/com/shumy/user/movieapp/MainActivity.java
+++ app/src/main/java/com/shumy/user/movieapp/MainActivity.java
@@ -15,11 +15,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final String TAG = "MainActivity";
 
-    // now_playing https://api.themoviedb.org/3/movie/now_playing?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1
-    // popular https://api.themoviedb.org/3/movie/popular?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1
-    // top_rated https://api.themoviedb.org/3/movie/top_rated?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1
-    // upcoming https://api.themoviedb.org/3/movie/upcoming?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1
-
     public static final String EXTRA_MESSAGE = "com.example.android.DisplayActivity.extra.MESSAGE";
 
     @Override
@@ -49,25 +44,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(view.getId()) {
             case R.id.popularButton:
                 intent = new Intent(this,DisplayActivity.class);
-                myURL = "https://api.themoviedb.org/3/movie/popular?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1";
+                myURL = "https://api.themoviedb.org/3/movie/popular?api_key=<<API_KEY>>&language=en-US&page=1";
                 Log.d(TAG, "onClick: popular button pressed with URL is " + myURL);
                 intent.putExtra(EXTRA_MESSAGE,myURL);
                 break;
             case R.id.playingButton:
                 intent = new Intent(this,DisplayActivity.class);
-                myURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1";
+                myURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=<<API_KEY>>&language=en-US&page=1";
                 Log.d(TAG, "onClick: playing button pressed with URL is " + myURL);
                 intent.putExtra(EXTRA_MESSAGE,myURL);
                 break;
             case R.id.ratedButton:
                 intent = new Intent(this,DisplayActivity.class);
-                myURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1";
+                myURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=<<API_KEY>>&language=en-US&page=1";
                 Log.d(TAG, "onClick: rated button pressed with URL is " + myURL);
                 intent.putExtra(EXTRA_MESSAGE,myURL);
                 break;
             case R.id.upcomingButton:
                 intent = new Intent(this,DisplayActivity.class);
-                myURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=d87c00b6a00ae74e930c063e106d6b4d&language=en-US&page=1";
+                myURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=<<API_KEY>>&language=en-US&page=1";
                 Log.d(TAG, "onClick: upcoming button pressed with URL is " + myURL);
                 intent.putExtra(EXTRA_MESSAGE,myURL);
                 break;
